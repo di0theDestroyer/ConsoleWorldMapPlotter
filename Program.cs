4f509db0967dc0eadc9402ebbcc67764d1106cfc
Dustin Johnson <dujohnson@osisoft.com>
2020-04-08 15:45:27 -0700

    well, PlotPoint() isn't quite working async yet, but it looks like it is

--- Program.cs
+++ Program.cs
@@ -15,60 +15,111 @@ namespace ConsoleWorldMapPlotter
         {
             //WorldMapPlotter worldMapPlotter = new WorldMapPlotter();
 
-            WorldMapPlotter.DrawMap();
-            WorldMapPlotter.StatusUpdate();
+            WorldMapPlotter.LoadMapFromFile();
+            WorldMapPlotter.RunMap();
+            //WorldMapPlotter.StatusUpdate();
+
+            Random rnd = new Random();
 
             // 2 options
+            while (true)
+            {
+                var x = rnd.Next(0, 30);
+                var y = rnd.Next(0, 30);
 
+                WorldMapPlotter.PlotPoint(x, y);
 
+                Thread.Sleep(1000);
+            }
 
+            Console.Read();
         }
 
         public static class WorldMapPlotter
         {
-            public static void DrawMap()
+
+            private static string _mapContents = string.Empty;
+
+            public static void LoadMapFromFile()
             {
                 // read map from .txt file
-
-                // output to console writer
-
                 string filePath = @"../../worldmap.txt";
-                string fileText = File.ReadAllText(filePath);
-
-                ConsoleWriter.WriteLine(fileText);
+                _mapContents = File.ReadAllText(filePath);
             }
 
+
+            public static void DrawMap()
+            {
+                //ConsoleWriter.WriteLine(_mapContents);
+            }
+
+            public static void RunMap()
+            {
+                Task.Run(() =>
+                {
+                    Random rnd = new Random();
+
+                    while (true)
+                    {
+                        var toDraw = new Tuple<string, int, int>(_mapContents, 0, 0);
+
+                        ConsoleWriter.Write(toDraw);
+
+                        var x = rnd.Next(0, 30);
+                        var y = rnd.Next(0, 30);
+                        PlotPoint(x , y);
+
+                        Thread.Sleep(1000);
+                    }
+                });
+            }
+
+            /*
             public static void StatusUpdate()
             {
-                var whiteSpace = new StringBuilder();
-                whiteSpace.Append(' ', 10);
-                var random = new Random();
-                const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                var randomWord = new string(Enumerable.Repeat(chars, random.Next(10)).Select(s => s[random.Next(s.Length)]).ToArray());
-
-                while (true)
+                Task.Run(() =>
                 {
-                    Console.SetCursorPosition(0, 0);
-                    var sb = new StringBuilder();
-                    sb.AppendLine($"Program Status:{whiteSpace}");
-                    sb.AppendLine("-------------------------------");
-                    sb.AppendLine($"Last Updated: {DateTime.Now}{whiteSpace}");
-                    sb.AppendLine($"Random Word: {randomWord}{whiteSpace}");
-                    sb.AppendLine("-------------------------------");
-                    ConsoleWriter.Write(sb.ToString());
-                    Thread.Sleep(1000);
-                }
+
+                    var whiteSpace = new StringBuilder();
+                    whiteSpace.Append(' ', 10);
+                    var random = new Random();
+                    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                    var randomWord = new string(Enumerable.Repeat(chars, random.Next(10)).Select(s => s[random.Next(s.Length)]).ToArray());
+
+                    while (true)
+                    {
+                        Console.SetCursorPosition(0, 0);
+
+                        var sb = new StringBuilder();
+                        sb.AppendLine($"Program Status:{whiteSpace}");
+                        sb.AppendLine("-------------------------------");
+                        sb.AppendLine($"Last Updated: {DateTime.Now}{whiteSpace}");
+                        sb.AppendLine($"Random Word: {randomWord}{whiteSpace}");
+                        sb.AppendLine("-------------------------------");
+
+                        ConsoleWriter.Write(sb.ToString());
+
+                        Thread.Sleep(1000);
+                    }
+                });
             }
+            */
 
-            public static void PlotPoint()
+            public static void PlotPoint(int x, int y)
             {
+                var toPlot = new Tuple<string, int, int>("@", x, y);
 
+                ConsoleWriter.Write(toPlot);
             }
         }
 
         public static class ConsoleWriter
         {
-            private static BlockingCollection<string> blockingCollection = new BlockingCollection<string>();
+            //private static BlockingCollection<string> writeLines = 
+            //    new BlockingCollection<string>();
+
+            private static BlockingCollection<Tuple<string, int, int>> writes =
+                new BlockingCollection<Tuple<string, int, int>>();
 
             static ConsoleWriter()
             {
@@ -76,20 +127,37 @@ namespace ConsoleWorldMapPlotter
                 {
                     while (true)
                     {
-                        System.Console.WriteLine(blockingCollection.Take());
+                        var toWrite = writes.Take();
+
+                        Console.SetCursorPosition(toWrite.Item2, toWrite.Item3);
+
+                        Console.Write(toWrite.Item1);
                     }
 
                 });
+
+                /*
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        Console.WriteLine(writes.Take());
+                    }
+
+                });
+                */
             }
 
+            /*
             public static void WriteLine(string value)
             {
-                blockingCollection.Add(value);
+                writeLines.Add(value);
             }
+            */
 
-            public static void Write(string value)
+            public static void Write(Tuple<string, int, int> value)
             {
-                blockingCollection.Add(value);
+                writes.Add(value);
             }
 
         }
